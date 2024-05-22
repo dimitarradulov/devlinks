d3525cdc5d33be0739215b8ba872d1bb95e6ec88
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-05-22 16:33:44 +0300

    add angularfire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'devlinks-fe-mentor',
+        appId: '1:445522180211:web:84726f8f5c73c45b349206',
+        storageBucket: 'devlinks-fe-mentor.appspot.com',
+        apiKey: 'AIzaSyDsDDBicoGG9VHorOcg5P6OgmftndmtcdU',
+        authDomain: 'devlinks-fe-mentor.firebaseapp.com',
+        messagingSenderId: '445522180211',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
