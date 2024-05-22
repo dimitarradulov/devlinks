fb02fb540d4548f5742b237adcc47d4bf480e911
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-05-22 16:24:44 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-60": "#633CFF",
+        "primary-85": "#BEADFF",
+        "primary-95": "#EFEBFF",
+        "secondary-20": "#333333",
+        "secondary-45": "#737373",
+        "secondary-85": "#D9D9D9",
+        "secondary-98": "#FAFAFA",
+        danger: "#FF3939",
+      },
+      fontSize: {
+        "heading-m": [
+          "2rem",
+          {
+            lineHeight: "150%",
+            fontWeight: "700",
+          },
+        ],
+        "heading-s": [
+          "1rem",
+          {
+            lineHeight: "150%",
+            fontWeight: "700",
+          },
+        ],
+        "body-m": [
+          "1rem",
+          {
+            lineHeight: "150%",
+            fontWeight: "400",
+          },
+        ],
+        "body-s": [
+          "0.75rem",
+          {
+            lineHeight: "150%",
+            fontWeight: "400",
+          },
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
