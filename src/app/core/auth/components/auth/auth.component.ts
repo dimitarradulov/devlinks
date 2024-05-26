0d4a6443e55ec6ee2235f780595257ce5a819e2b
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-05-26 14:56:21 +0300

    import common module

--- src/app/core/auth/components/auth/auth.component.ts
+++ src/app/core/auth/components/auth/auth.component.ts
@@ -5,17 +5,19 @@ import {
   output,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { BoxComponent } from '../../../../shared/components/box/box.component';
 import { InputComponent } from '../../../../shared/components/input/input.component';
 import { LogoComponent } from '../../../../shared/components/logo/logo.component';
 import { AuthCredentials } from '../../models/auth.model';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
   imports: [
+    CommonModule,
     FormsModule,
     RouterModule,
     BoxComponent,
