0833d9b8f7ea5e487ab774b6734e0e400990a48a
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-06-02 15:20:04 +0300

    add basic html & styling to customize

--- src/app/features/links/pages/customize/customize.component.ts
+++ src/app/features/links/pages/customize/customize.component.ts
@@ -1,15 +1,23 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Auth, signOut } from '@angular/fire/auth';
 
 import { LoadingComponent } from '../../../../shared/components/loading/loading.component';
+import { BoxComponent } from '../../../../shared/components/box/box.component';
+import { IconComponent } from '../../../../shared/components/icon/icon.component';
 
 @Component({
   selector: 'app-customize',
   standalone: true,
   templateUrl: './customize.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, LoadingComponent],
+  imports: [
+    CommonModule,
+    LoadingComponent,
+    BoxComponent,
+    IconComponent,
+    NgOptimizedImage,
+  ],
 })
 export class CustomizeComponent {
   private auth = inject(Auth);
